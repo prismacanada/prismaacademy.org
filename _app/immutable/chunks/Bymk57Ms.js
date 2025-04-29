e8ef52678d7f5c9b236107e49a72ce17ad4b2965
Harun Zafer <harunzafer.dev@gmail.com>
2025-04-29 13:01:05 -0400

    Updates

---
+++ _app/immutable/chunks/Bymk57Ms.js
@@ -0,0 +1,1 @@
+import{l as a}from"./6nwpymF-.js";a();
---
+++ _app/immutable/nodes/6.BiyOLqPB.js
@@ -0,0 +1,8 @@
+import{t as c,a as p}from"../chunks/BGcnAg6G.js";import"../chunks/Bymk57Ms.js";import{s as d,f as m,c as o,r as t,n as h,g as e}from"../chunks/6nwpymF-.js";import{e as v,i as u,a as w,E as b}from"../chunks/CGI7AiiN.js";var f=c(`<div class="section event-3-banner"><div class="base-container w-container"><div class="banner-event-wrapper"><h1 class="light-text">Upcoming and Past Events</h1> <p class="paragraph-large banner-event-3-paragraph">Stay connected with our community through inspiring events that foster learning, mentorship,
+				and collaboration. From career panels and skill workshops to social gatherings, explore
+				opportunities to grow, engage, and make an impact.</p></div></div></div> <section class="section"><div class="w-layout-blockcontainer base-container w-container"><ul role="list" class="list"></ul></div> <section><div class="w-layout-blockcontainer base-container w-container"><img src="images/2nd-gathering.jpg" loading="lazy" sizes="(max-width: 1439px) 100vw, 1330px" srcset="
+			images/2nd-gathering-p-500.jpg 500w,
+			images/2nd-gathering-p-800.jpg 800w,
+			images/2nd-gathering-p-1080.jpg 1080w,
+			images/2nd-gathering.jpg 1600w
+		  " alt="Second Gathering"></div></section></section>`,1);function U(g){var n=f(),r=d(m(n),2),i=o(r),s=o(i);v(s,5,()=>w,u,(l,a)=>{b(l,{get date(){return e(a).date},get title(){return e(a).title},get description(){return e(a).description},get imageUrl(){return e(a).imageUrl},get imageAlt(){return e(a).imageAlt},get learnMoreUrl(){return e(a).learnMoreUrl}})}),t(s),t(i),h(2),t(r),p(g,n)}export{U as component};
